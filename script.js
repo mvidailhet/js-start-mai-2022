c5e2d57f4dc8fd00b8a0545d62c24170f9d8a835
Michel Vidailhet <michel.vidailhet@gmail.com>
2022-07-11 15:03:13 +0200

    exo du bonjour "Mitch"

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log("test");
+// Ce programme doit dire bonjour s'il reconnaît le nom, sinon "go away"
+// Faire marcher avec "Antoine" et "Alex"
+const userName = prompt("Quel est ton nom ?");
+
+const lowerUserName = userName.toLowerCase();
+
+const accceptedNames = ["paul", "mitch", "antoine", "alex"];
+
+if (accceptedNames.includes(lowerUserName)) {
+  console.log("Welcome, " + userName);
+} else {
+  console.log("Go away!");
+}
+
+
+/* if (
+  lowerUserName === "paul" ||
+  lowerUserName === "mitch" ||
+  lowerUserName === "antoine" ||
+  lowerUserName === "alex"
+) {
+  console.log("Welcome, " + userName);
+} else {
+  console.log("Go away!");
+} */
